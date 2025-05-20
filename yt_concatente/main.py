39749ba252ed3c1d11fae302855e65fe072d1f59
itsmeru <cat821016@gmail.com>
2025-05-20 14:21:29 +0800

    Refactor code into pipeline design pattern

--- yt_concatente/main.py
+++ yt_concatente/main.py
@@ -1,33 +1,21 @@
-import json
-import urllib.request
-from settings import API_KEY
+from pipeline.step.get_vedio_list import GetVedioList
+from pipeline.pipeline import Pipeline
 
-channel_id = "UC_mYaQAE6-71rjSN6CeCA-g"
-
-
-def process():
-
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
-
-    first_url = f'{base_search_url}key={API_KEY}&channelId={channel_id}&part=snippet,id&order=date&maxResults=25'
-
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except KeyError:
-            break
-    return video_links
+CHANNEL_ID = "UC_mYaQAE6-71rjSN6CeCA-g"
 
 
-print(process())
+def main():
+    steps = [
+        GetVedioList(),
+    ]
+
+    inputs = {
+        'channel_id': CHANNEL_ID
+    }
+
+    p = Pipeline(steps)
+    p.run(inputs)
+
+
+if __name__ == '__main__':
+    main()
